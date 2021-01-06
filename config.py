47d1c50f2d4c9c727cd3745db824714c40fd88b7
Brooke Cooper <techanalyst65@gmail.com>
2021-01-06 00:33:22 -0500

    Uploading gitignore file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Cloud / Salesforce Info
+sf_username = 'techanalyst65@gmail.com'
+sf_password = 'UqS@720826'
+sf_security_token = 'e2U9R5x7urg3BK2jLv2PJDGZ'
+
+# Cloud / MySQL Info
+remote_db_endpoint = 'databootcamp.cv1u8ipt6nbb.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'postgres123'
+remote_db_name = 'gwsis'
